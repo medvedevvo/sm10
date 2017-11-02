c6622b455e8285005dad6eb396a0fb06f73f4095
medvedevvo <vladrobot@mail.ru>
2017-11-02 13:54:35 +0300

    Add events for buttons

--- StateMonitorApp/StateMonitorApp/StateMonitorApp/MainPage.xaml.cs
+++ StateMonitorApp/StateMonitorApp/StateMonitorApp/MainPage.xaml.cs
@@ -23,6 +23,11 @@ namespace StateMonitorApp
 
         private void btnCurrentState_Clicked(object sender, EventArgs e)
         {
+            lblTest.Text = "1";
+
+        }
+        private void btnStatistics_Clicked(object sender, EventArgs e)
+        {
             press_cnt++;
             lblTest.Text = "Заработало!!! Уже " + press_cnt.ToString() + " нажати";
             if (((press_cnt % 10) >= 5) || ((press_cnt % 10) == 0) || ((press_cnt >= 10) && (press_cnt <= 20)))
@@ -34,6 +39,21 @@ namespace StateMonitorApp
 
             lblTest.Text += "!";
 
+        }
+
+        private void btnGraphics_Clicked(object sender, EventArgs e)
+        {
+            lblTest.Text = "3";
+        }
+
+        private void btnSettings_Clicked(object sender, EventArgs e)
+        {
+            lblTest.Text = "4";
+        }
+
+        private void btnAbout_Clicked(object sender, EventArgs e)
+        {
+            lblTest.Text = "5";
         }
     }
 }
