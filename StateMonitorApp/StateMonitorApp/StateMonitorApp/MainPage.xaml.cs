6bed74a7d3b97571bb6cc0dee31acbf74aac5ed2
medvedevvo <vladrobot@mail.ru>
2017-11-02 13:19:26 +0300

    Add cross-platform phone App base

---
+++ StateMonitorApp/StateMonitorApp/StateMonitorApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StateMonitorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private Label lblTest;
+        private int press_cnt = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            lblTest = this.FindByName<Label>("_lblTest");
+
+        }
+
+        private void btnCurrentState_Clicked(object sender, EventArgs e)
+        {
+            press_cnt++;
+            lblTest.Text = "Заработало!!! Уже " + press_cnt.ToString() + " нажати";
+            if (((press_cnt % 10) >= 5) || ((press_cnt % 10) == 0) || ((press_cnt >= 10) && (press_cnt <= 20)))
+                lblTest.Text += "й";
+            else if ((press_cnt % 10) == 1)
+                lblTest.Text += "е";
+            else
+                lblTest.Text += "я";
+
+            lblTest.Text += "!";
+
+        }
+    }
+}
